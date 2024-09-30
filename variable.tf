12ef7640d08cf057ce860a268622a939e24c897b
ikiaTaruiM <163708652+ikiaTaruiM@users.noreply.github.com>
2024-10-01 08:42:28 +0900

    Add the file of "variable.tf"

---
+++ variable.tf
@@ -0,0 +1,35 @@
+variable "resource_group" {    
+}
+
+variable "location" {
+}
+
+variable "virtual_network" {
+}
+
+variable "subnet" {
+}
+
+variable "public_ip" {
+}
+
+variable "availability_set" {
+}
+
+variable "storage_account" {
+}
+
+variable "recovery_services_vault" {
+}
+
+variable "backup_policy_vm_daily" {
+}
+
+variable "backup_policy_vm_weekly" {
+}
+
+variable "proximity_placement_group" {
+}
+
+variable "backup_storage_account" {
+}
