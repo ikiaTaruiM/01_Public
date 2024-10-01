929f1089b41a9a5c601954021234e129ab2a61b6
ikiaTaruiM <163708652+ikiaTaruiM@users.noreply.github.com>
2024-10-01 23:23:25 +0900

    Add files via upload

---
+++ 02_Server/02.1_Terraform/Terraform-project/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "resource_group_test"
+}
+
+variable "location" {
+  default = "Japan East"
+}
+
+variable "password" {
+  default = "Root@123456789"
+}
